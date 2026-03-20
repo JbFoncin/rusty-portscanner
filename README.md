54f1c3632a24fa488ea03de732c30581c5e58972
jbfoncin <jeanbaptiste.foncin@gmail.com>
2026-03-20 09:59:15 +0000

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rusty-portscanner
 A simple asynchronous port scanner written in Rust
+
+Use with cli : one argument, the ip adress to scan 
+example : cargo run 127.0.0.1
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::net::{IpAddr, SocketAddr};
+use std::str::FromStr;
+use std::time::Duration;
+use tokio::task::JoinHandle;
+use tokio::net::TcpStream;
+use tokio::time::{timeout};
+
+async fn connect_to_addr(ip_to_scan: IpAddr, port: u16) -> Option<u16> {
+    let socket_addr: SocketAddr = SocketAddr::new(ip_to_scan, port.clone());
+        if let Ok(_stream) = TcpStream::connect(&socket_addr).await {
+            Some(port.clone())
+        } else {
+            //println!("connection to {} with port {} failed", &ip_to_scan, &port);
+            None
+        }
+}
+
+async fn loop_over_all_values(ip_addr: &str) -> Vec<u16> {
+    let mut open_ports: Vec<u16> = Vec::with_capacity(1000);
+    let mut tasks: Vec<JoinHandle<Option<u16>>> = Vec::with_capacity(65535);
+    for port in 0..=65535_u16 {
+        let timeout_duration: Duration = std::time::Duration::from_millis(100000); //big timeout because most servers restrict too much connexions
+        let ip_to_scan: IpAddr = IpAddr::from_str(&ip_addr).unwrap();
+        let fut = timeout(timeout_duration, connect_to_addr(ip_to_scan, port.clone()));
+        let task: JoinHandle<Option<u16>> = tokio::task::spawn(async move { if let Ok(result) = fut.await { result }
+                                                                                   else { None } });
+        tasks.push(task);
+    }
+    for task in tasks {
+        if let Some(port) = task.await.unwrap() { open_ports.push(port); }
+    }
+    open_ports
+}
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let ip_to_scan: String = args[1].clone();
+    let open_ports: Vec<u16> = loop_over_all_values(&ip_to_scan).await;
+    println!("{:?}", &open_ports)
+}
